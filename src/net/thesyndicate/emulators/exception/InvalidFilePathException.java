b83ee58c5421e1d949ab9286ab32691f4b741643
John Lewis <dragon99x@hotmail.com>
2014-10-23 07:48:54 -0500

    Setting up exceptions.

--- src/net/thesyndicate/emulators/exception/InvalidFilePathException.java
+++ src/net/thesyndicate/emulators/exception/InvalidFilePathException.java
@@ -3,5 +3,5 @@ package net.thesyndicate.emulators.exception;
 /**
  * Created by johlewis on 10/23/2014.
  */
-public class InvalidFilePathException {
+public class InvalidFilePathException extends EmulatorException {
 }
